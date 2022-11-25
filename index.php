d902256500cd4e34b6f75033e7ef8f2806358813
giuliagrfo <giuliagrifo1@gmail.com>
2022-11-25 15:51:25 +0100

    add index - form

--- index.php
+++ index.php
@@ -9,8 +9,21 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 </head>
 <body>
-    <div class="container">
-        
-    </div>
+        <div class="container mt-5">
+            <form>
+                <div class="form-group w-50 mb-3">
+                    <label for="paragraph" class="form-label text-uppercase">Input Paragraph</label>
+                        <input type="text" class="form-control" name="inputName" id="inputName" placeholder="">
+                </div>
+                <div class="form-group w-50 mb-3">
+                    <label for="paragraph" class="form-label text-uppercase">Input A Bad Word</label>
+                        <input type="text" class="form-control" name="inputName" id="inputName" placeholder="">
+                </div>
+                <div class="form-group">
+                        <button type="submit" class="btn btn-primary">Submit</button>
+                        <button type="submit" class="btn btn-danger">Delete</button>
+                </div>
+            </form>
+        </div>
 </body>
 </html>
