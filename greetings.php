b7167c5bf57fb80ba00fbd43b3ffece577369d7f
giuliagrfo <giuliagrifo1@gmail.com>
2022-11-25 16:27:44 +0100

    working on greetings

--- greetings.php
+++ greetings.php
@@ -9,6 +9,18 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 </head>
 <body>
-   
+
+    <?php
+
+    $paragraph = $_GET['paragraph'];
+    $badword = $_GET['word'];
+    echo $paragraph .' is ' .strlen($paragraph) .' characters long' .'<br>'; 
+
+    $censored = str_ireplace($badword, '***', $paragraph);
+    echo $censored . ' is '. strlen($censored) . ' characters long';
+
+
+    ?>
+
 </body>
 </html>
--- index.php
+++ index.php
@@ -9,21 +9,21 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 </head>
 <body>
-        <div class="container mt-5">
-            <form>
-                <div class="form-group w-50 mb-3">
-                    <label for="paragraph" class="form-label text-uppercase">Input Paragraph</label>
-                        <input type="text" class="form-control" name="inputName" id="inputName" placeholder="">
-                </div>
-                <div class="form-group w-50 mb-3">
-                    <label for="paragraph" class="form-label text-uppercase">Input A Bad Word</label>
-                        <input type="text" class="form-control" name="inputName" id="inputName" placeholder="">
-                </div>
-                <div class="form-group">
-                        <button type="submit" class="btn btn-primary">Submit</button>
-                        <button type="submit" class="btn btn-danger">Delete</button>
-                </div>
-            </form>
-        </div>
+    <div class="container mt-5">
+        <form action="greetings.php" method="get">
+            <div class="form-group w-50 mb-3">
+                <label for="paragraph" class="form-label text-uppercase">Input Paragraph</label>
+                <input type="text" class="form-control" name="paragraph" id="paragraph">
+            </div>
+            <div class="form-group w-50 mb-3">
+                <label for="word" class="form-label text-uppercase">Input A Bad Word</label>
+                <input type="text" class="form-control" name="word" id="word">
+            </div>
+            <div class="form-group">
+                <button type="submit" class="btn btn-primary" type="submit">Submit</button>
+                <button type="submit" class="btn btn-danger"  type="delete">Delete</button>
+            </div>
+        </form>
+    </div>
 </body>
 </html>
